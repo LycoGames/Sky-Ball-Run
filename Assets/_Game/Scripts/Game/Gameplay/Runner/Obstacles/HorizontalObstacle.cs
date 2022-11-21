93836e0c852996121e9aff95e6251a020404b318
Enes Gündüz <56505397+Deaglis1197@users.noreply.github.com>
2022-11-21 21:47:12 +0300

    Color Buggy

--- Assets/_Game/Scripts/Game/Gameplay/Runner/Obstacles/HorizontalObstacle.cs
+++ Assets/_Game/Scripts/Game/Gameplay/Runner/Obstacles/HorizontalObstacle.cs
@@ -1,11 +1,28 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 namespace _Game.Scripts.Game.Gameplay.Runner.Obstacles
 {
     public class HorizontalObstacle : MonoBehaviour
     {
-       
+        [SerializeField] private Material material;
+        [SerializeField] private float colorChangeTime = .3f;
+        
+        private void OnEnable()
+        {
+            Color color = material.color;
+            color.a = 255;
+            material.color = color;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                material.DOFade(0, colorChangeTime);
+            }
+        }
     }
 }
